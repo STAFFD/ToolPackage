2d1494423eb6ac7711a38622b8398a29d721b59c
STAFFD <sheldoneinstein123@gmail.com>
2021-07-08 12:44:03 +0800

    update

--- pyTool/cvision/src/adaptiveThreshold.py
+++ pyTool/cvision/src/adaptiveThreshold.py
@@ -1,14 +1,41 @@
 from cv2 import *
 from cvision.src.utils import *
+import numpy as np
 
-def func(image):
-    image = cv_scale(image, 0.5)
-    ret, thresh1 = threshold(image, 127, 255, THRESH_BINARY)
-    ret,thresh2 = threshold(image,127,255,THRESH_BINARY_INV)
-    ret,thresh3 = threshold(image,127,255,THRESH_TRUNC)
-    ret,thresh4 = threshold(image,127,255,THRESH_TOZERO)
-    ret,thresh5 = threshold(image,127,255,THRESH_TOZERO_INV)
-    return [thresh1, thresh2, thresh3, thresh4, thresh5]
+def func(img):
+    img = cv_scale(img, 0.5)
+    img = cvtColor(img, COLOR_BGR2GRAY)
+    ret, thresh1 = threshold(img, 127, 255, THRESH_BINARY)
+    ret,thresh2 = threshold(img,127,255,THRESH_BINARY_INV)
+    ret,thresh3 = threshold(img,127,255,THRESH_TRUNC)
+    ret,thresh4 = threshold(img,127,255,THRESH_TOZERO)
+    ret,thresh5 = threshold(img,127,255,THRESH_TOZERO_INV)
+    imHeight, imWidth = img.shape
+    print("imHeight:{}".format(imHeight))
+    print("imWidth:{}".format(imWidth))
+
+    for col in range(0, imWidth, 4):
+        for row in range(0, imHeight, 4):
+            m = img[row:row+4, col:col+4].mean()
+            a = img[row:row+4, col:col+4]
+            a = np.where(a>m, a, 255)
+            a = np.where(a==255, a, 0)
+            img[row:row+4, col:col+4] = a
+    return img, thresh1, thresh2
+
+def scale_grey(image):
+    return cvtColor(cv_scale(image, 0.5), COLOR_BGR2GRAY)
+
+def adaptiveThreshold(image):
+    return image
+
+def boundarySegmentation(image):
+    return image
 
 if __name__ == "__main__":
-    cv_do(IMG_QR, func)
+    # image = np.random.randint(255, size=(800,800,3),dtype=np.uint8)
+    # cv_do(image, func)
+
+    cv_do(IMG_QR, [ scale_grey,
+                    adaptiveThreshold, 
+                    boundarySegmentation])
--- pyTool/cvision/src/utils.py
+++ pyTool/cvision/src/utils.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 IMG_QR = "pyTool/cvision/media/QR.jpg"
 
@@ -14,12 +15,38 @@ def cv_scale(image, scaler):
     imHeight, imWidth, channels = image.shape
     return cv2.resize(image, (int(imWidth*scaler), int(imHeight*scaler)))
 
-def cv_do(imagePath, function):
-    images = function(cv2.imread(imagePath))
-    if type(images) == list:
-        for idx, image in enumerate(images):
-            cv2.imshow("{}_{}".format(function.__name__, idx), 
-                        image)
+def cv_do(img, function=None, show=True):
+    """
+        img: the input image. 
+            Can be the path to the image (str) or image data (np.ndarray)
+        function: the operation to the image. 
+                Function must take and return an image. 
+                Can be one function or list of function.
+        show: should show the image.
+    """
+    if type(img) == str:
+        img = cv2.imread(img)
+    if function is not None:
+        if type(function) == list:
+            images = img
+            for func in function:
+                images = func(images)
+            name = function[-1].__name__
+        else:
+            images = function(img)
+            name = function.__name__
     else:
-        cv2.imshow(function.__name__, images)
+        images = img
+        name = ""
+    if not show:
+        return images
+    if type(images) == list or type(images) == tuple:
+        for idx, image in enumerate(images):
+            cv2.imshow("{}_{}".format(name, idx), 
+                        image)
+
+    elif type(images) == np.ndarray:
+        cv2.imshow(name, images)
+    else:
+        return
     cv_quitHold()
