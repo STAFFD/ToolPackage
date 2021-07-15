b5eebfea9009a66b8ea07f9a987803430d3a5089
STAFFD <sheldoneinstein123@gmail.com>
2021-07-15 14:47:37 +0800

    update

--- pyTool/plts/example/plotGaussian.py
+++
@@ -1,7 +0,0 @@
-from plts.func.plot2D import plot2DFormular
-import numpy as np
-
-def gaussian(x, sig=0.5, mu=0.5):
-    return 1/np.sqrt(2*np.pi*np.square(sig))*np.exp(-(np.square(x-mu)/(2*np.square(sig))))
-
-plot2DFormular(gaussian, fRange=[-5, 5])
