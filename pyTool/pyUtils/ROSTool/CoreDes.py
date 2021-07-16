45c34a13024f5bc37a8f8071285b152a4288bf30
Sheldon Von <724530250@qq.com>
2021-07-16 12:42:08 +0800

    Update

--- pyTool/pyUtils/ROSTool/CoreDes.py
+++ pyTool/pyUtils/ROSTool/CoreDes.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import subprocess
 import argparse
 import json
 import os
@@ -10,12 +9,11 @@ parser.add_argument('-ip', type=str, default="localhost")
 parser.add_argument('-s', action='store_true', default=False)
 parser.add_argument('-r', action='store_true', default=False)
 args = parser.parse_args()
-
+path = os.path.dirname(os.path.realpath(__file__))
 core = {"IP": args.ip}
 if args.s and args.r:
 	print("Can not save and restore at the same time")
 if args.s or args.r:
-	path = os.path.dirname(os.path.realpath(__file__))
 	with open('{}/data.json'.format(path), 
 				'w' if args.s else 'r' , 
 				encoding='utf-8') as f:
@@ -25,11 +23,12 @@ if args.s or args.r:
 		else:
 			core = json.load(f)
 			print("Core IP loaded -> {}".format(core["IP"]))
-
-ROS_MASTER_URI = "export ROS_MASTER_URI=http://{}:11311".format(core["IP"])
-ROS_IP = 'export ROS_IP={}'.format(core["IP"])
-print(ROS_MASTER_URI)
-subprocess.call(ROS_IP, shell=True)
-print(ROS_IP)
-subprocess.call(ROS_MASTER_URI, shell=True)
-print("Done!")
+ROS_MASTER_URI = "export ROS_MASTER_URI=http://{}:11311\n".format(core["IP"])
+ROS_IP = 'export ROS_IP={}\n'.format(core["IP"])
+with open("{}/CoreDes.sh".format(path), "w") as file:
+	file.write("#!/bin/sh\n")
+	file.write("echo ROS IP is now set to {}\n".format(core["IP"]))
+	file.write(ROS_MASTER_URI)
+	file.write(ROS_IP)
+	file.write("rm {}/CoreDes.sh".format(path))
+print("Run the following commands:\n\n. CoreDes.sh")
